cd5c329afff175da1a38b51e959430b5c2b8a83c
SakibHasan <sakibhasan911@gmail.com>
2021-09-10 02:15:54 +0600

    first comment

---
+++ random.js
@@ -0,0 +1,7 @@
+let num = 2.4999999999999999;
+let result = Math.floor(num);
+let result2 = Math.ceil(num);
+let result3 = Math.round(num);
+// console.log(result);
+// console.log(result2);
+// console.log(result3);
